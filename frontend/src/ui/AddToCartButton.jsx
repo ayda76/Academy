58a70f0b9162710eeb5ed078e2f8517614a7c35b
tara-sabori <tarasaboori@gmail.com>
2025-11-22 20:23:40 +0330

    add add to cart button

---
+++ frontend/src/ui/AddToCartButton.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import { useCart } from "../context/CartContext";
+import useUser from "../hooks/auth/useUser";
+
+const AddToCartButton = ({ id, name, price }) => {
+  const { user, isLoadingUser } = useUser();
+  const { cartList, dispatch } = useCart();
+  const isExistInCart = cartList?.findIndex((cart) => cart?.id === id);
+  console.log(isExistInCart);
+  return (
+    <div className="w-[200px] lg:w-full">
+      {user?.id && user?.status === "isRegister" ? (
+        <div className="w-full p-1.5 rounded-xl text-center bg-purple-900">
+          <span className="text-xs text-white md:text-sm">
+            شما دانشجو دوره هستید
+          </span>
+        </div>
+      ) : isExistInCart === -1 ? (
+        <button
+          onClick={() =>
+            dispatch({ type: "addedToCart", payload: { id, name, price } })
+          }
+          className="w-full p-1.5 cursor-pointer rounded-xl text-center bg-purple-900"
+        >
+          <span className="text-xs text-white md:text-sm">
+            افزودن به سبد سفارش
+          </span>
+        </button>
+      ) : (
+        <Link
+          to={"/cart"}
+          className="w-full p-1.5 cursor-pointer rounded-xl text-center inline-block bg-purple-900"
+        >
+          <span className="text-xs text-white md:text-sm">
+            پرداخت و شروع یادگیری
+          </span>
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default AddToCartButton;
