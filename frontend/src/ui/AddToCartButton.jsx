b85cb47e584ac45bbff72eac7c8884a49fae1f3d
tara-sabori <tarasaboori@gmail.com>
2025-12-01 01:43:16 +0330

    add enroll button for online course

--- frontend/src/ui/AddToCartButton.jsx
+++ frontend/src/ui/AddToCartButton.jsx
@@ -1,16 +1,21 @@
 import { Link } from "react-router-dom";
 import { useCart } from "../context/CartContext";
-import useUser from "../hooks/auth/useUser";
 import toast from "react-hot-toast";
+import useAuth from "../hooks/useAuth";
 
 const AddToCartButton = ({ id, name, price }) => {
-  const { user, isLoadingUser } = useUser();
+  // const { user, isLoadingUser } = useUser();
+  const { myCourse, isLoadingCourse, isLoadingUser, user } = useAuth();
   const { cartList, dispatch } = useCart();
+  const course = !myCourse || myCourse === "error" ? [] : myCourse;
+  const isEnroll = course?.some((c) => c?.id === id);
   const isExistInCart = cartList?.findIndex((cart) => cart?.id === id);
   console.log(isExistInCart);
   return (
     <div className="w-[200px] lg:w-full">
-      {user?.id && user?.status === "isRegister" ? (
+      {isLoadingCourse || isLoadingUser ? (
+        <div className="w-full p-4 rounded-xl bg-secondary-200 animate-pulse"></div>
+      ) : user?.id && isEnroll ? (
         <div className="w-full p-1.5 rounded-xl text-center bg-purple-900">
           <span className="text-xs text-white md:text-sm">
             شما دانشجو دوره هستید
---
+++ frontend/src/ui/EnrollButton.jsx
@@ -0,0 +1,58 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import toast from "react-hot-toast";
+import useEnroll from "../hooks/courses/useEnroll";
+
+const EnrollButton = ({ courseId, courseName }) => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  console.log(pathname);
+  const { myCourse, isLoadingCourse, isLoadingUser, user } = useAuth();
+  const course = !myCourse || myCourse === "error" ? [] : myCourse;
+  const isEnroll = course?.some((c) => c?.id === courseId);
+  const { enrollCourseFn, isPending } = useEnroll();
+
+  const enrollHandler = () => {
+    if (!user?.id) {
+      navigate("/auth/signin", { state: { from: pathname } });
+      toast.error("برای ثبت‌نام در این دوره وارد حساب کاربری خود شوید.");
+      return;
+    } else if (!user?.firstname) {
+      navigate("/auth/complete-profile", { state: { from: pathname } });
+      toast.error("برای ثبت‌نام در این دوره اطلاعات خود را تکمیل کنید.");
+      return;
+    }
+    const formData = {
+      term_id: courseId,
+    };
+    enrollCourseFn(formData, {
+      onSuccess: () => {
+        toast.success(`دوره ${courseName} باموفقیت ثیت‌نام شد`);
+        navigate("/dashboard/course");
+      },
+    });
+  };
+  return (
+    <div className="w-[200px] lg:w-full">
+      {isLoadingCourse || isLoadingUser ? (
+        <div className="w-full p-4 rounded-xl bg-secondary-200 animate-pulse"></div>
+      ) : user?.id && isEnroll ? (
+        <div className="w-full p-1.5 rounded-xl text-center bg-purple-900">
+          <span className="text-xs text-white md:text-sm">
+            شما دانشجو دوره هستید
+          </span>
+        </div>
+      ) : (
+        <button
+          onClick={enrollHandler}
+          disabled={isPending}
+          className="w-full p-1.5 cursor-pointer rounded-xl text-center bg-purple-900 disabled:cursor-not-allowed disabled:bg-secondary-400"
+        >
+          <span className="text-xs text-white md:text-sm">ثبت‌نام</span>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default EnrollButton;
